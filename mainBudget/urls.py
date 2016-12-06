143534ec29d570772cacaa8b0a14ca3e18def33b
Rishab shrestha <Rishab shrestha>
2016-12-06 13:51:51 -0600

    budget

--- mainBudget/urls.py
+++ mainBudget/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from dueDates import views
 
 urlpatterns = [
-    url(r'^budget/$', views.dates.as_view()),
-    url(r'^budget/(?P<pk>[0-9]+)/$', views.dueDatesDetail.as_view()),
+    url(r'^budget/$', views.mBudget.as_view()),
+    url(r'^budget/(?P<pk>[0-9]+)/$', views.mbudgetDetail.as_view()),
 ]
