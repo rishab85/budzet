24f0c352a07c724928aaeb1ffe3e9fd7e3962a54
Rishab shrestha <Rishab shrestha>
2016-12-05 19:31:39 -0600

    last

--- budzet/wsgi.py
+++ budzet/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
+from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budzet.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
